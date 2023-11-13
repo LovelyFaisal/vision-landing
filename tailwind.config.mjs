8999f1ccce85159c5dbd938707148d61c4f6dcb5
LovelyFaisal <45786166+LovelyFaisal@users.noreply.github.com>
2023-11-13 18:15:19 +0300

    init landing page

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,12 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
+		container: {
+			center: true,
+			screens: {
+				'2xl': '1336px'
+			}
+		}
 	},
 	plugins: [],
 }
