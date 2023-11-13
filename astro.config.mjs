feb650b0d693523063fb23dcc3d5de228af63c7a
LovelyFaisal <45786166+LovelyFaisal@users.noreply.github.com>
2023-11-13 18:27:34 +0300

    deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://lovelyfaisal.github.io',
+  base: 'vision-landing',
   integrations: [tailwind()]
 });
